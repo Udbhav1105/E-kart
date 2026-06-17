3a39bde61f66f7baf14450fa06d2d8ff15d7df16
Udbhav1105 <udbhavmsr@gmail.com>
2026-06-17 21:27:37 +0530

    email updated

--- backend/verifyemail/verifyEmail.js
+++ backend/verifyemail/verifyEmail.js
@@ -3,21 +3,21 @@ import nodemailer from'nodemailer';
 export const verifyEmail=async(token,email)=>{
   console.log("yaha pr bhi aagya")
   const transporter = await nodemailer.createTransport({
-    service:'gmail',
-    port:465,
-    secure:true,
+    service:'smtp.gmail.com',
+    port:587,
+    secure:false,
     auth: {
     user: process.env.EMAIL,
     pass: process.env.PASS,
   },
 });
 console.log("ye aagya to upar waala bhi hogya")
-try {
-  await transporter.verify();
-  console.log("SMTP Connected");
-} catch (err) {
-  console.log("SMTP Error:", err);
-}
+// try {
+//   await transporter.verify();
+//   console.log("SMTP Connected");
+// } catch (err) {
+//   console.log("SMTP Error:", err);
+// }
 console.log("ye aaya kya?")
 
 try {
