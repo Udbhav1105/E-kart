4871184cae698a912737710053b58538d9eef360
Udbhav1105 <udbhavmsr@gmail.com>
2026-06-17 21:20:48 +0530

    email updated

--- backend/verifyemail/verifyEmail.js
+++ backend/verifyemail/verifyEmail.js
@@ -20,18 +20,21 @@ try {
 }
 console.log("ye aaya kya?")
 
-(async () => {
-  const info = await transporter.sendMail({
-    from: process.env.EMAIL,
-    to: email,
-    subject: "Email Verification",
-    text: `You have recently visited our website please verify your account by clicking on https://e-kart-2-77mr.onrender.com/verify/${token}`, // Plain-text version of the message
-    
-  });
-  console.log(email,"yaha pr bhi aagya ab kya dikkat hai?")
+try {
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL,
+      to: email,
+      subject: "Email Verification",
+      text: `https://e-kart-2-77mr.onrender.com/verify/${token}`,
+    });
 
-  console.log("Message sent:", info);
-})();
+    console.log("MAIL SENT");
+    console.log(info.response);
+
+  } catch (err) {
+    console.log("SEND MAIL ERROR");
+    console.log(err);
+  }
 
 }
 
