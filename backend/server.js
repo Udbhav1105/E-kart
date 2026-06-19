c0bc12dc47cd43f25cbc596e567dedd7f31d4380
Udbhav1105 <udbhavmsr@gmail.com>
2026-06-19 23:32:01 +0530

    resend integrated

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(
 // app.get('/',(req,res)=>{
 //     res.sendFile(path.join(process.cwd(), "public", "index.html"));
 // })
-app.get("*", (req, res) => {
+app.get((req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 app.use('/api/v1/user',userRoute)
